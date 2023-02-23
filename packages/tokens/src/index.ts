12fef1fe4352984e4268126d5452c894ecfabfba
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-02-23 08:34:34 -0300

    ds: package tokens

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,11 @@
+export * from './colors'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+
+export * from './line-heights'
+export * from './radii'
+export * from './space'
+
+// import { colors } from '@ignite-ds/tokens'
